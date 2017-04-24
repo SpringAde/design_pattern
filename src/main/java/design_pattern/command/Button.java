322cc53c4fccb3afe2d02beb8017bb96c9d1968c
inyoung <inyoung.estrella6y@naver.com>
2017-04-24 11:41:37 +0900

    커맨드 패턴 - 버튼 만들기

---
+++ src/main/java/design_pattern/command/Button.java
@@ -0,0 +1,30 @@
+package design_pattern.command;
+
+public class Button {
+	private Lamp theLamp;
+	private Alarm theAlarm;
+	private Mode theMode;
+	
+	public Button(Lamp theLamp, Alarm theAlarm) {
+		this.theLamp = theLamp;
+		this.theAlarm = theAlarm;
+	}
+
+	public void setTheMode(Mode theMode) {
+		this.theMode = theMode;
+	}
+	
+	public void pressed(){
+		switch(theMode){
+		case LAMP :
+			theLamp.turnOn();
+			break;
+		case ALARM : 
+			theAlarm.start();
+			break;
+		}
+	}
+	
+	
+	
+}
---
+++ src/main/java/design_pattern/command/Mode.java
@@ -0,0 +1,5 @@
+package design_pattern.command;
+
+public enum Mode {
+	LAMP, ALARM;
+}
